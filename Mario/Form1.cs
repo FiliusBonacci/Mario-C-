f27faf2e2868c8698d4753694880c48c48c555f9
Pawel Jadanowski <pjadanowski@gmail.com>
2017-03-10 23:35:51 +0100

    first commit

---
+++ Mario/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mario
+{
+    public partial class Form1 : Form
+    {
+        bool right;
+        bool left;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (right) player.Left += 5;
+            if (left) player.Left -= 5;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) left = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) right = false;
+            if (e.KeyCode == Keys.Left) left = false;
+        }
+    }
+}
