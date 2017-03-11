3561b008850c211c5aa640cb79c2de813caf2ae3
Pawel Jadanowski <pjadanowski@gmail.com>
2017-03-11 11:07:58 +0100

    z przeszkodą

--- Mario/Form1.Designer.cs
+++ Mario/Form1.Designer.cs
@@ -29,26 +29,29 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.scene = new System.Windows.Forms.Panel();
             this.player = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.panel1.SuspendLayout();
+            this.block = new System.Windows.Forms.PictureBox();
+            this.scene.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // scene
             // 
-            this.panel1.Controls.Add(this.player);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1094, 631);
-            this.panel1.TabIndex = 0;
+            this.scene.Controls.Add(this.block);
+            this.scene.Controls.Add(this.player);
+            this.scene.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scene.Location = new System.Drawing.Point(0, 0);
+            this.scene.Name = "scene";
+            this.scene.Size = new System.Drawing.Size(1094, 631);
+            this.scene.TabIndex = 0;
             // 
             // player
             // 
             this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.player.Location = new System.Drawing.Point(251, 495);
+            this.player.Location = new System.Drawing.Point(240, 248);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(25, 25);
             this.player.TabIndex = 0;
@@ -60,29 +63,40 @@
             this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.block.Location = new System.Drawing.Point(481, 550);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(304, 81);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1094, 631);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.scene);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Mario - Paweł Jadanowski";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            this.panel1.ResumeLayout(false);
+            this.scene.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel scene;
         private System.Windows.Forms.PictureBox player;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox block;
     }
 }
 
--- Mario/Form1.cs
+++ Mario/Form1.cs
@@ -14,6 +14,9 @@ namespace Mario
     {
         bool right;
         bool left;
+        bool jump;
+        int gravity = 30;
+        int Force;
 
         public Form1()
         {
@@ -27,14 +30,61 @@ namespace Mario
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+
+            // kolizja boczna
+            if (player.Right > block.Left && player.Left < block.Right - player.Width
+                && player.Bottom >block.Top)
+                right = false;
+            if (player.Left < block.Right && player.Right > block.Left + player.Width
+                && player.Bottom > block.Top)
+                left = false;
+
+
+
+            // ruch
             if (right) player.Left += 5;
             if (left) player.Left -= 5;
+            if (jump) // jezeli byl skok
+            {
+                player.Top -= Force;
+                Force--;
+            }
+            if (player.Top + player.Height >= scene.Height)
+            {
+                player.Top = scene.Height - player.Height; // zatrzymaj spadanie na podlodze
+                jump = false;
+            }
+            else
+            {
+                player.Top += 5; 
+            }
+
+            // kolizja gorna
+            if(player.Left + player.Width - 1 > block.Left  && player.Left + player.Width + 5 < block.Left + block.Width
+                 + player.Width
+                && player.Top + player.Height >= block.Top && player.Top < block.Top)
+            {
+                player.Top = scene.Height - block.Height - player.Height;
+                jump = false;
+                Force = 0;
+            }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Right) { right = true; }
             if (e.KeyCode == Keys.Left) left = true;
+            if (e.KeyCode == Keys.Escape) this.Close();
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    Force = gravity;
+                }
+            }
+
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
